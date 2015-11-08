b6267c17cfa33986856c155335b88e80ae6e9451
Dylan Waits <dylan@waits.io>
2015-11-08 15:46:39 -0800

    Add ip function

--- ip.go
+++ ip.go
@@ -1,7 +1,19 @@
 package main
 
+import "encoding/json"
 import "fmt"
+import "io/ioutil"
+import "net/http"
 
 func ip() {
-	fmt.Println("0.0.0.0")
+	resp, err := http.Get("https://bandwidth.waits.io/ip.json")
+	check(err)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var data map[string]interface{}
+	var jErr = json.Unmarshal(body, &data)
+	check(jErr)
+
+	fmt.Printf("%s\n", data["ip"])
 }
