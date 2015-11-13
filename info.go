59cb7696e91ff8b0002b63eaae9bfc232b191592
Dylan Waits <dylan@waits.io>
2015-11-13 11:42:14 -0800

    Add timezone and coordinates to info output

--- info.go
+++ info.go
@@ -27,12 +27,18 @@ func ShowInfo(args []string) {
 	}
 	info := GetInfo(url)
 	var location string
-	if info["city"] != nil {
-		location = fmt.Sprintf("%s, %s", info["city"], info["country"])
+	if info["city"] != nil && info["region"] != nil {
+		location = fmt.Sprintf("%s, %s, %s", info["city"], info["region"], info["country"])
 	} else {
 		location = fmt.Sprintf("%s", info["country"])
 	}
 	fmt.Printf("IPv4 address:\t%s\nHostname:\t%s\nLocation:\t%s\n", info["ip"], info["host"], location)
+	if info["latitude"] != nil && info["longitude"] != nil {
+		fmt.Printf("Coordinates:\t%f, %f\n", info["latitude"], info["longitude"])
+	}
+	if info["time_zone"] != nil {
+		fmt.Printf("Time zone:\t%s\n", info["time_zone"])
+	}
 }
 
 func ShowIp() {
