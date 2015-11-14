50683e229ccec57a52bfe315d519acb22ce7a51b
Dylan Waits <dylan@waits.io>
2015-11-13 18:19:15 -0800

    Add geo command and arguments to help text

--- help.go
+++ help.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func ShowHelp() {
-	fmt.Println("usage: ntwrk <command> [options]")
+	fmt.Println("usage: ntwrk <command> [arguments]\n")
 	fmt.Println("commands:")
-	commands := [3]string{"help", "info", "ip"}
-	descriptions := [3]string{"Show help message for a command",
-		"Show detailed network information",
+	commands := [4]string{"geo [ip]", "help", "info [ip]", "ip"}
+	descriptions := [4]string{
+		"Show GeoIP data about an IP",
+		"Show this help message",
+		"Show basic info about an IP",
 		"\tShow external IPv4 address"}
 	for i, cmd := range commands {
 		fmt.Printf("    %v\t%v\n", cmd, descriptions[i])
